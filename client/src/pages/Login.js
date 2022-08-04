76bf79381c3f90b95059abc70ec158fc67ffc78f
rkutsel <roman.kutsel@docusign.com>
2022-08-03 23:38:57 -0700

    login && signup pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,71 +1,92 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { Link } from 'react-router-dom';
-import { LOGIN } from '../utils/mutations';
-import Auth from '../utils/auth';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { LOGIN } from "../utils/mutations";
+import Auth from "../utils/auth";
+
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Stack from "react-bootstrap/Stack";
+import Card from "react-bootstrap/Card";
 
 function Login(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
-  const [login, { error }] = useMutation(LOGIN);
+	const [formState, setFormState] = useState({ email: "", password: "" });
+	const [login, { error }] = useMutation(LOGIN);
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const mutationResponse = await login({
-        variables: { email: formState.email, password: formState.password },
-      });
-      const token = mutationResponse.data.login.token;
-      Auth.login(token);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+	const handleFormSubmit = async (event) => {
+		event.preventDefault();
+		try {
+			const mutationResponse = await login({
+				variables: { email: formState.email, password: formState.password },
+			});
+			const token = mutationResponse.data.login.token;
+			Auth.login(token);
+		} catch (e) {
+			console.log(e);
+		}
+	};
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setFormState({
+			...formState,
+			[name]: value,
+		});
+	};
+	const navigate = useNavigate();
 
-  return (
-    <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
+	return (
+		<div>
+			<Container>
+				<Row className="justify-content-center">
+					<Col xs={7}>
+						<Card>
+							<Card.Body>
+								<Form onSubmit={handleFormSubmit}>
+									<Form.Group className="mb-3">
+										<Form.Label>Email address</Form.Label>
+										<Form.Control
+											type="email"
+											placeholder="youremail@test.com"
+											name="email"
+											id="email"
+											onChange={handleChange}
+										/>
+									</Form.Group>
 
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        {error ? (
-          <div>
-            <p className="error-text">The provided credentials are incorrect</p>
-          </div>
-        ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
-  );
+									<Form.Group className="mb-3">
+										<Form.Label>Password</Form.Label>
+										<Form.Control
+											placeholder="Password"
+											name="password"
+											type="password"
+											id="pwd"
+											onChange={handleChange}
+										/>
+									</Form.Group>
+									<Stack gap={2} className="col-md-5 mx-auto">
+										<Button variant="outline-success" type="submit">
+											Log in
+										</Button>
+										<Button
+											onClick={() => navigate("/signup")}
+											variant="outline-primary"
+											type="button"
+										>
+											Sign up
+										</Button>
+									</Stack>
+								</Form>
+							</Card.Body>
+						</Card>
+					</Col>
+				</Row>
+			</Container>
+		</div>
+	);
 }
 
 export default Login;
