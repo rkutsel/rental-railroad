93c702d8cdbe3193ebbd52889808b47090612cb7
Raji Marimuthu <raji@modeanalytics.com>
2022-08-03 18:59:54 -0700

    Changed the quotes

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,12 +3,14 @@ const { User, Product, Category, Order } = require('../models');
 const { signToken } = require('../utils/auth');
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
+
 const resolvers = {
   Query: {
     categories: async () => {
       return await Category.find();
     },
-    
+
+
     products: async (parent, { category, name }) => {
       const params = {};
 
