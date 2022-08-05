b9e3b8e692f758862c4996d2ec32520de6d73828
unknown <scarletfedora@gmail.com>
2022-08-05 13:17:56 -0700

    Styling navbar w/ react-bootstrap

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,41 +1,47 @@
 import React from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
+import "./index.css";
 
 // import Container from "react-bootstrap/esm/Container";
 // import Nav from "react-bootstrap/esm/Nav";
 // import Navbar from "react-bootstrap/esm/Navbar";
 // import NavDropdown from "react-bootstrap/esm/NavDropdown";
 
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown, Stack } from "react-bootstrap";
 
 function Navigation() {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-      <Container>
+    <Navbar className="Navbar" variant="dark" expand="lg" sticky="top">
+      <Container className="ms-auto">
+        <Navbar.Brand as={Link} to="/">
+              Rental Railroad!
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav>
-            <Navbar.Brand as={Link} to="/">
-              Rental Railroad!
-            </Navbar.Brand>
-            <NavDropdown title="Category" id="collapsible-nav-dropdown">
-              <NavDropdown.Item as={Link} to="/">
-                Roman
-              </NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/">
-                Courtney
-              </NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/">
-                Logan
-              </NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/">
-                Raji
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Navbar.Brand>
-              <Link to="/login">Login</Link>
-            </Navbar.Brand>
+          <Nav className="ms-auto align-items-start">
+            <Stack direction="horizontal" gap={5}>
+              <Navbar.Brand className="p-2" as={Link} to="/">
+                Home
+              </Navbar.Brand>
+              <NavDropdown className="p-2" title="Category" id="collapsible-nav-dropdown">
+                <NavDropdown.Item as={Link} to="/">
+                  Roman
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/">
+                  Courtney
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/">
+                  Logan
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/">
+                  Raji
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Navbar.Brand className="p-2" as={Link} to="/login">
+                Login
+              </Navbar.Brand>
+            </Stack>
           </Nav>
         </Navbar.Collapse>
       </Container>
