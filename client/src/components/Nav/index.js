00e9a3d899348d948d7d79fe4ac4af5cd4fa00c5
Logan Monson <scarletfedora@gmail.com>
2022-08-05 02:50:30 -0700

    Got navbar to collapse and style!

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,32 +2,42 @@ import React from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
 
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+// import Container from "react-bootstrap/esm/Container";
+// import Nav from "react-bootstrap/esm/Nav";
+// import Navbar from "react-bootstrap/esm/Navbar";
+// import NavDropdown from "react-bootstrap/esm/NavDropdown";
+
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 function Navigation() {
   return (
-    <Navbar fixed="top" expand="lg">
+    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
       <Container>
-        <Navbar.Brand>
-          <Link to="/">Rental Railroad!</Link>
-        </Navbar.Brand>
-        {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-        {/* <Navbar.Collapse id="basic-navbar-nav"> */}
-        <Nav className="me-auto">
-          <NavDropdown title="Category" id="collapsible-nav-dropdown">
-            <NavDropdown.Item>Roman</NavDropdown.Item>
-            <NavDropdown.Item>Courtney</NavDropdown.Item>
-            <NavDropdown.Item>Logan</NavDropdown.Item>
-            <NavDropdown.Item>Raji</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Navbar.Brand>
-          <Link to="/login">Login</Link>
-        </Navbar.Brand>
-        {/* </Navbar.Collapse> */}
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav>
+            <Navbar.Brand as={Link} to="/">
+              Rental Railroad!
+            </Navbar.Brand>
+            <NavDropdown title="Category" id="collapsible-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/">
+                Roman
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/">
+                Courtney
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/">
+                Logan
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/">
+                Raji
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Navbar.Brand>
+              <Link to="/login">Login</Link>
+            </Navbar.Brand>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
