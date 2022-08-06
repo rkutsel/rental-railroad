50af73f823628e88bd5dd3d30deb5a24be8bb9dd
Logan Monson <scarletfedora@gmail.com>
2022-08-06 14:29:54 -0700

    styling navbar again

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -18,41 +18,27 @@ function Navigation() {
     return (
       <Navbar className="Navbar" variant="dark" expand="lg" sticky="top">
         <Container className="d-flex justify-content-between">
-          <Navbar.Brand className="Brand" as={Link} to="/">
+          <Navbar.Brand className="Brand nav-text" as={Link} to="/">
             <Image className="Logo" src={Logo} />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ms-auto align-items-start">
               <Stack direction="horizontal" gap={5}>
-                <Navbar.Brand className="p-2 d-flex hover" as={Link} to="/">
+                <Navbar.Brand
+                  className="p-2 d-flex hover nav-text"
+                  as={Link}
+                  to="/"
+                >
                   Home
                 </Navbar.Brand>
-                {/* <NavDropdown
-                  className="p-2 hover"
-                  title="Category"
-                  id="collapsible-nav-dropdown"
-                >
-                  <NavDropdown.Item as={Link} to="/">
-                    Roman
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/">
-                    Courtney
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/">
-                    Logan
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/">
-                    Raji
-                  </NavDropdown.Item>
-                </NavDropdown> */}
                 <CategoryMenu />
                 <Navbar.Brand className="p-2 hover" as={Link} to="/profile">
                   Profile
                 </Navbar.Brand>
                 <Navbar.Brand
                   onClick={() => Auth.logout()}
-                  className="p-2 hover"
+                  className="p-2 hover nav-text"
                   as={Link}
                   to="/"
                 >
@@ -68,35 +54,23 @@ function Navigation() {
     return (
       <Navbar className="Navbar" variant="dark" expand="lg" sticky="top">
         <Container className="d-flex justify-content-between">
-          <Navbar.Brand className="Brand" as={Link} to="/">
+          <Navbar.Brand className="Brand nav-text" as={Link} to="/">
             <Image className="Logo" src={Logo} />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ms-auto align-items-start">
               <Stack direction="horizontal" gap={5}>
-                <Navbar.Brand className="p-2 hover" as={Link} to="/">
+                <Navbar.Brand className="p-2 hover nav-text" as={Link} to="/">
                   Home
                 </Navbar.Brand>
-                <NavDropdown
-                  className="p-2 hover"
-                  title="Category"
-                  id="collapsible-nav-dropdown"
+
+                <CategoryMenu />
+                <Navbar.Brand
+                  className="p-2 hover nav-text"
+                  as={Link}
+                  to="/login"
                 >
-                  <NavDropdown.Item as={Link} to="/">
-                    Roman
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/">
-                    Courtney
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/">
-                    Logan
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/">
-                    Raji
-                  </NavDropdown.Item>
-                </NavDropdown>
-                <Navbar.Brand className="p-2 hover" as={Link} to="/login">
                   Login
                 </Navbar.Brand>
               </Stack>
