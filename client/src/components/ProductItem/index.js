c050ed70b527968b84d623f3021ce9df0bf708c9
Courtney Struelens <courtneystruelens@hotmail.com>
2022-08-07 14:25:07 -0700

    updated card styling

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -8,33 +8,32 @@ import "./styles.css";
 import { Component } from "react";
 
 export default class Card extends Component {
-	render() {
-		let img;
-		if (this.props.image) {
-			img = <img src={"/images/" + this.props.image} alt={this.props.name} />;
-		} else {
-			img = (
-				<img
-					src="https://dash-bootstrap-components.opensource.faculty.ai/static/images/placeholder286x180.png"
-					alt=""
-				/>
-			);
-		}
+  render() {
+    let img;
+    if (this.props.image) {
+      img = <img src={"/images/" + this.props.image} alt={this.props.name} />;
+    } else {
+      img = (
+        <img
+          src="https://dash-bootstrap-components.opensource.faculty.ai/static/images/placeholder286x180.png"
+          alt=""
+        />
+      );
+    }
 
-		return (
-			<div className="Card">
-				<Link to={`/products/${this.props._id}`}>{img}</Link>
-				<div className={"Card-body " + this.props.color}>
-					<h2>
-						{this.props.name} &nbsp; $
-						{(Math.round(this.props.price * 100) / 100).toFixed(2)} per day
-					</h2>
-					<p>{this.props.description}</p>
-					{/* <h5>
-						${(Math.round(this.props.price * 100) / 100).toFixed(2)} per day
-					</h5> */}
-				</div>
-			</div>
-		);
-	}
+    return (
+      <div className="Card">
+        <Link to={`/products/${this.props._id}`}>{img}</Link>
+        <div className={"Card-body " + this.props.color}>
+          <div className="d-flex justify-content-between">
+            <h2 className="d-inline">{this.props.name}</h2>
+            <span>
+              ${(Math.round(this.props.price * 100) / 100).toFixed(2)} per day
+            </span>
+          </div>
+          <p>{this.props.description}</p>
+        </div>
+      </div>
+    );
+  }
 }
