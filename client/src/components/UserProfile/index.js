93548138cbaa4b455dd72a5cc1824044e1c5028b
Raji Marimuthu <raji@modeanalytics.com>
2022-08-07 14:23:10 -0700

    Merging the user info comonent

--- client/src/components/UserProfile/index.js
+++ client/src/components/UserProfile/index.js
@@ -1,16 +1,40 @@
 import React from "react";
-import Auth from "../../utils/auth";
 import Image from 'react-bootstrap/Image';
-import Card from 'react-bootstrap/Card';
 import Container from "react-bootstrap/Container";
+import { Col, Row } from "react-bootstrap";
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../../utils/queries';
+import avatarImg from "../../assets/user_avatar.png";
 
-function UserProfileInfo(props) {
-    if (Auth.loggedIn()) {
+
+
+function UserProfileInfo() {
+
+    const { loading, data } = useQuery(QUERY_USER);
+    
+    const userprofile = data?.user || {};
+
+    if (userprofile) {
         return(
-            <Container>
-                <Row className="justify-content-center">
-                     <Image className="Logo" src={Logo} roundedCircle/>
-                     <Card body>${props.aboutMe}</Card>;
+            <Container my-3>
+                <Row className="d-flex 
+                flex-lg-row 
+                flex-md-column 
+                flex-sm-column 
+                justify-content-lg-center 
+                justify-content-md-center
+                justify-content-sm-center 
+                align-items-lg-center
+                align-items-md-center
+                align-items-sm-center
+                align-content-sm-center
+                m-5 p-5" >
+                    <Col xs={12} md={6} lg={3} >
+                        <Image className="border border-warning p-2" src={avatarImg} roundedCircle width={200} height={200} />
+                     </Col>
+                     <Col xs={12} md={6} lg={9}>
+                            <p className = "text-center px-3 m-3">{userprofile.aboutMe}</p>
+                     </Col>
                 </Row>
             </Container>
         );
