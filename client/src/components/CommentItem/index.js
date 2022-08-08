8aaa658b11a72d39d4bc36a8e3f40c7ebda1ce22
Courtney Struelens <courtneystruelens@hotmail.com>
2022-08-07 17:52:41 -0700

    added comment components and fixed bug in card component overlay

--- client/src/components/CommentItem/index.js
+++ client/src/components/CommentItem/index.js
@@ -1,29 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 // Styles
 import "./styles.css";
 
-function CommentList() {
+function CommentItem() {
   return (
-    <li class="media">
-      <span class="round pt-2">
+    <li className="media">
+      <span className="round pt-2">
         <img
           src="https://img.icons8.com/bubbles/100/000000/groups.png"
-          class="align-self-start mr-3"
+          className="align-self-start mr-3"
         />
       </span>
-      <div class="media-body">
-        <div class="row d-flex">
-          <h6 class="user pt-2">Michael Andrews</h6>
-          <div class="ml-auto">
-            <p class="text">3m</p>
+      <div className="media-body">
+        <div className="row d-flex">
+          <h6 className="user pt-2">{this.props.author}</h6>
+          <div className="ml-auto">
+            <p className="text">{this.props.createdAt}</p>
           </div>
         </div>
-        <p class="text">Liked four of your artworks</p>
+        <p className="text">{this.props.comment}</p>
       </div>
     </li>
   );
 }
 
-export default CommentList;
+export default CommentItem;
--- client/src/components/CommentList/index.js
+++ client/src/components/CommentList/index.js
@@ -1,32 +1,55 @@
 import React from "react";
 import CommentItem from "../CommentItem";
 import { useQuery } from "@apollo/client";
-import { QUERY_PRODUCTS } from "../../utils/queries";
-import { Link } from "react-router-dom";
+import { QUERY_SINGLE_PRODUCT } from "../../utils/queries";
 
 // Styles
 import "./styles.css";
 
 function CommentList() {
+  const { loading, data } = useQuery(QUERY_SINGLE_PRODUCT);
+
+  const comments = data?.product.comments || [];
+
+  console.log(data);
+
   return (
-    <div className="commentList">
-      <div class="container">
-        <div class="row">
-          <h3>Comments</h3>
-        </div>
-      </div>
-      <div class="container mt-1">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="card">
-              <ul class="list-unstyled">
-                <CommentItem />
-              </ul>
-            </div>
+    <>
+      <div className="commentList">
+        <div className="container">
+          <div className="row">
+            <h3>Comments</h3>
           </div>
         </div>
+        {comments.length ? (
+          <div className="container mt-1">
+            <div className="row">
+              <div className="col-md-12">
+                <div className="card">
+                  <ul className="list-unstyled">
+                    <CommentItem
+                      author={comments.author}
+                      comment={comments.comment}
+                      createdAt={comments.createdAt}
+                    />
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="container mt-1">
+            <div className="row">
+              <div className="col-md-12">
+                <div className="card">
+                  <h5>No comments yet!</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
-    </div>
+    </>
   );
 }
 
