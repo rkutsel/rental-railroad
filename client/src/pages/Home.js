86ffd6fc85f5b36c61125c6d4a8510d8bbfb2646
Logan Monson <scarletfedora@gmail.com>
2022-08-08 21:30:53 -0700

    Category header

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,42 @@
 import React from "react";
 import ProductList from "../components/ProductList";
-// import CategoryMenu from "../components/CategoryMenu";
-import Cart from "../components/Cart";
+import { useStoreContext } from "../utils/GlobalState";
+import { QUERY_CATEGORY } from "../utils/queries";
+import { useQuery } from "@apollo/client";
+import Container from "react-bootstrap/Container";
+
+// import Cart from "../components/Cart";
 
 const Home = () => {
-  return (
-    <div className="container">
-      {/* <CategoryMenu /> */}
-      <ProductList />
-      {/* <Cart /> */}
-    </div>
-  );
+  const [state, dispatch] = useStoreContext();
+
+  const { currentCategory } = state;
+
+  const { loading, data } = useQuery(QUERY_CATEGORY, {
+    variables: { categoryId: `${currentCategory}` },
+  });
+
+  const category = data?.category || [];
+
+  console.log(category);
+
+  if (!currentCategory) {
+    return (
+      <Container>
+        <h1 className="display-2 text-center">
+          Take a look at these <small className="text-muted">hot</small> items!
+        </h1>
+        <ProductList />
+      </Container>
+    );
+  } else {
+    return (
+      <Container>
+        <h1 className="display-2 text-center">{category.name}!</h1>
+        <ProductList />
+      </Container>
+    );
+  }
 };
 
 export default Home;
